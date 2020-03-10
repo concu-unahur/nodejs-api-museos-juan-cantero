5807d681a59a18b9862704cf1cc426dac9ba30d9
juan-cantero <juan.cantero@outlook.com>
2020-03-10 20:18:46 -0300

    museos

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 const superagent = require('superagent');
+const log = console.log;
+const chalk = require('chalk');
+const fs = require('fs');
 
-function imprimirMuseos(error, respuesta) {
-  if (error) {
-    throw new Error('algo se rompió', error);
-  }
 
-  const cantidad = respuesta.body.count;
-  const museos = respuesta.body.results;
 
-  console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+
+
+
+function escribirDatosMuseo(error, res) {
+
+  const museos = res.body.results;
+
+  const datos = museos.map(m => {
+    return ('museos.txt', `${m.nombre} (${m.direccion}). por cualquier consulta comunicarse al ${m.telefono} \n`);
+  });
+
+  fs.writeFile('museos2.txt', datos, (error) => {
+    if (error) {
+      throw new Error('algo se rompió', error);
+    }
+  })
+
+
 }
 
-console.log('Antes de llamar a superagent')
+
+function imprimirDatosMuseo(error, res) {
+
+  const museos = res.body.results;
+
+  museos.forEach(museo => {
+
+    //desestructuracion de objetos
+    const { nombre, direccion, telefono } = museo;
+
+    fs.appendFile('museos.txt', `${nombre} (${direccion}). por cualquier consulta comunicarse al ${telefono} \n`,
+      () => {
+        if (error)
+          throw new Error("ups");
+      });
+  })
+
+}
+
 
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
   .query({ format: 'json' })
-  .end(imprimirMuseos)
+  .end(escribirDatosMuseo);
 
-console.log('Después de llamar a superagent')
+
+
